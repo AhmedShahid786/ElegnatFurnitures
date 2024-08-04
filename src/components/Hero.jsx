46ae1db570dededa79348d77f8018b75205b7e0d
AhmedShahid786 <154593144+AhmedShahid786@users.noreply.github.com>
2024-08-05 02:21:09 +0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
       <div className="container mx-auto flex px-5 pt-14 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-amber-700">
-            Discover Our Latest Collection of
+            Discover Our Latest Collection of 
             <br className="hidden lg:inline-block" />
             Elegant Furniture
           </h1>
